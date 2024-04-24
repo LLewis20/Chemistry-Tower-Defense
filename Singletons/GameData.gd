a048205c27b1241ef2f573347534ee5d64ac1c6a
Littlep640 <78877689+Littlep640@users.noreply.github.com>
2024-04-24 10:17:03 -0500

    Removed UniCode For Itch Support
    Changed questions to remove unicode so game may be playable.

--- Singletons/GameData.gd
+++ Singletons/GameData.gd
@@ -42,7 +42,7 @@ var questions = {
 	## ACID | Base | POLYATOMIC | Neither Questions
 	##
 	"1":{
-		"Question": "What is BaCrO\u2084?",
+		"Question": "What is BaCrO4",
 		"Answer": "Salt",
 		"Options": {
 			"1": "Acid",
@@ -64,7 +64,7 @@ var questions = {
 	},
 
 	"3":{
-		"Question": "What is CCl\u2084?",
+		"Question": "What is CCl4",
 		"Answer": "Neither",
 		"Options": {
 			"1": "Acid",
@@ -75,7 +75,7 @@ var questions = {
 	},
 
 	"4":{
-		"Question": "What is HCLO\u2084?",
+		"Question": "What is HCLO4",
 		"Answer": "Acid",
 		"Options": {
 			"1": "Acid",
@@ -86,7 +86,7 @@ var questions = {
 	},
 
 	"5":{
-		"Question": "What is HNO\u2082?",
+		"Question": "What is HNO2",
 		"Answer": "Acid",
 		"Options": {
 			"1": "Base",
@@ -97,7 +97,7 @@ var questions = {
 	},
 
 	"6":{
-		"Question": "What is C\u2083H\u2088",
+		"Question": "What is C3H8",
 		"Answer": "Neither",
 		"Options": {
 			"1": "Acid",
@@ -108,7 +108,7 @@ var questions = {
 	},
 
 	"7":{
-		"Question": "What is CH\u2083COOH?",
+		"Question": "What is CH3COOH?",
 		"Answer": "Acid",
 		"Options": {
 			"1": "Acid",
@@ -119,7 +119,7 @@ var questions = {
 	},
 
 	"8":{
-		"Question": "What is MgC\u2082O\u2084?",
+		"Question": "What is MgC2O4",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -141,7 +141,7 @@ var questions = {
 	},
 
 	"10":{
-		"Question": "What is CH\u2084",
+		"Question": "What is CH4?",
 		"Answer": "Neither",
 		"Options": {
 			"1": "Acid",
@@ -152,7 +152,7 @@ var questions = {
 	},
 
 	"11":{
-		"Question": "What is CaCO\u2083?",
+		"Question": "What is CaCO3",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -163,7 +163,7 @@ var questions = {
 	},
 
 	"12":{
-		"Question": "What is K\u2082SO\u2083",
+		"Question": "What is K2SO3",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -174,7 +174,7 @@ var questions = {
 	},
 
 	"13":{
-		"Question": "What is NO\u2083",
+		"Question": "What is NO3",
 		"Answer": "Polyatomic",
 		"Options": {
 			"1": "Acid",
@@ -185,7 +185,7 @@ var questions = {
 	},
 
 	"14":{
-		"Question": "What is SO\u2084",
+		"Question": "What is SO4",
 		"Answer": "Polyatomic",
 		"Options": {
 			"1": "Acid",
@@ -196,7 +196,7 @@ var questions = {
 	},
 
 	"15":{
-		"Question": "What is NO\u2082",
+		"Question": "What is NO2",
 		"Answer": "Polyatomic",
 		"Options": {
 			"1": "Acid",
@@ -210,61 +210,61 @@ var questions = {
 	## BALANCE EQUATIONS
 	##
 	"16":{
-		"Question": "What is the products and balance for the acid-base equation KNO\u2082 (aq) + HClO\u2084 (aq) \u2192 ?",
-		"Answer": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq)",
+		"Question": "What is the products and balance for the acid-base equation KNO2 (aq) + HClO4 (aq) 2 ?",
+		"Answer": "HNO2 (aq) + K (aq) + ClO4 (aq)",
 		"Options": {
-			"1": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq)",
-			"2": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq) + H\u2082O (l)",
-			"3": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq) + H\u2082O (l) + O\u2082 (g)",
+			"1": "HNO2 (aq) + K (aq) + ClO4 (aq)",
+			"2": "HNO2 (aq) + K (aq) + ClO4 (aq) + H2O (l)",
+			"3": "HNO2 (aq) + K (aq) + ClO4 (aq) + H2O (l) + O2 (g)",
 			"4": "No Reaction"
 		}
 	},
 
 	"17":{
-		"Question": "What is the products and balance for the acid-base equation KNO\u2083 (aq) + HClO\u2084 (aq) \u2192",
+		"Question": "What is the products and balance for the acid-base equation KNO3 (aq) + HClO4 (aq) 2",
 		"Answer": "No Reaction",
 		"Options": {
-			"1": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq)",
-			"2": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq) + H\u2082O (l)",
-			"3": "HNO\u2082 (aq) + K (aq) + ClO\u2084 (aq) + H\u2082O (l) + O\u2082 (g)",
+			"1": "HNO2 (aq) + K (aq) + ClO4 (aq)",
+			"2": "HNO2 (aq) + K (aq) + ClO4 (aq) + H2O (l)",
+			"3": "HNO2 (aq) + K (aq) + ClO4 (aq) + H2O (l) + O2 (g)",
 			"4": "No Reaction"
 		}
 	},
 
 	"18":{
-		"Question": "What is the products and balance for the acid-base equation HCl (aq) + H\u2082O (l) \u2192",
-		"Answer": "H\u2083O\u2084 (aq) + Cl (aq)",
+		"Question": "What is the products and balance for the acid-base equation HCl (aq) + H2O (l) 2",
+		"Answer": "H3O4 (aq) + Cl (aq)",
 		"Options": {
-			"1": "H\u2083O\u2084 (aq) + Cl (aq) + O\u2082 (g)",
-			"2": "H\u2083O\u2084 (aq) + Cl (aq)",
-			"3": "H\u2083O\u2084 (aq) + Cl (aq) + O\u2082 (g) + H\u2082O (l)",
+			"1": "H3O4 (aq) + Cl (aq) + O2 (g)",
+			"2": "H3O4 (aq) + Cl (aq)",
+			"3": "H3O4 (aq) + Cl (aq) + O2 (g) + H2O (l)",
 			"4": "No Reaction"
 		}
 	},
 
 	"19":{
-		"Question": "What is the products and balance for the acid-base equation HCN (aq) + H\u2082O (l) \u2192",
-		"Answer": "H\u2083O (aq) + CN (aq)",
+		"Question": "What is the products and balance for the acid-base equation HCN (aq) + H2O (l) 2",
+		"Answer": "H3O (aq) + CN (aq)",
 		"Options": {
-			"1": "H\u2083O (aq) + CN (aq) + O\u2082 (g)",
-			"2": "H\u2083O (aq) + CN (aq)",
-			"3": "H\u2083O (aq) + CN (aq) + O\u2082 (g) + H\u2082O (l)",
+			"1": "H3O (aq) + CN (aq) + O2 (g)",
+			"2": "H3O (aq) + CN (aq)",
+			"3": "H3O (aq) + CN (aq) + O2 (g) + H2O (l)",
 			"4": "No Reaction"
 		}
 	},
 
 	"20":{
-		"Question": "What is the products and balance for the acid-base equation KClO\u2084 (aq) + HBr (aq) \u2192",
+		"Question": "What is the products and balance for the acid-base equation KClO4 (aq) + HBr (aq) 2",
 		"Answer": "No Reaction",
 		"Options": {
-			"1": "HClO\u2084 (aq) + KBr (aq)",
-			"2": "HClO\u2084 (aq) + KBr (aq) + H\u2082O (l)",
-			"3": "HClO\u2084 (aq) + KBr (aq) + H\u2082O (l) + O\u2082 (g)",
+			"1": "HClO4 (aq) + KBr (aq)",
+			"2": "HClO4 (aq) + KBr (aq) + H2O (l)",
+			"3": "HClO4 (aq) + KBr (aq) + H2O (l) + O2 (g)",
 			"4": "No Reaction"
 		}
 	},
 	"21":{
-		"Question": "What is CH\u2083-NH\u2082",
+		"Question": "What is CH3-NH2",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -274,7 +274,7 @@ var questions = {
 		}
 	},
 	"22":{
-		"Question": "What is Mg(OH)\u2082",
+		"Question": "What is Mg(OH)2",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -294,7 +294,7 @@ var questions = {
 		}
 	},
 	"24":{
-		"Question": "What is HNO\u2083",
+		"Question": "What is HNO3",
 		"Answer": "Acid",
 		"Options": {
 			"1": "Acid",
@@ -304,7 +304,7 @@ var questions = {
 		}
 	},
 	"25":{
-		"Question": "What is K\u2083PO\u2084",
+		"Question": "What is K3PO4",
 		"Answer": "Base",
 		"Options": {
 			"1": "Acid",
@@ -314,7 +314,7 @@ var questions = {
 		}
 	},
 	"26":{
-		"Question": "What is KNO\u2083",
+		"Question": "What is KNO3",
 		"Answer": "Neither",
 		"Options": {
 			"1": "Acid",
@@ -324,7 +324,7 @@ var questions = {
 		}
 	},
 	"27":{
-		"Question": "What is KNO\u2082",
+		"Question": "What is KNO2",
 		"Answer": "Neither",
 		"Options": {
 			"1": "Acid",
